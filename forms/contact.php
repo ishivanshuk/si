6c2ae31bc188ffc0581f52578cd7c98024802ebd
shivanshu2410 <67194809+shivanshu2410@users.noreply.github.com>
2020-07-14 12:58:28 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,14 +1,16 @@
 <?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$subject = $_POST["subject"];
+	$content = $_POST["message"];
 
-
-  
-  $mailFrom = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
-  
- 
-  echo($subject);
- 
-
-
+	$toEmail = "shivanshukr24@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
 ?>
